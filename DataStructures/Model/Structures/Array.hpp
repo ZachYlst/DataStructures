0cd83978136043380f6a69104115118b2e72e67f
ZachYlst <ZachYlst@users.noreply.github.com>
2018-02-02 09:08:14 -0700

    Added more code to OOTAC.

--- DataStructures/Model/Structures/Array.hpp
+++ DataStructures/Model/Structures/Array.hpp
@@ -17,7 +17,69 @@ using namespace std;
 template <class Type>
 class Array
 {
+public:
+    template <class Type>
+    Array<Type> :: Array(int size)
+    {
+        assert(size > 0);
+        this->size = size;
+        
+        internalArray = new Type[size];
+    }
+    Array<Type>(int size);
     
+    template <class Type>
+    Array<Type> :: Array(const Array<Type> & toCopy)
+    {
+        this->size = toCopy.getSize();
+        
+        //Build Data Structure
+        internalArray = new Type[size];
+        
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toCopy[index];
+        }
+    }
+    Array<Type>(const Array<Type> & toCopy);
+    template <class Type>
+    Array<Type> :: ~Array()
+    {
+        delete[] internalArray;
+    }
+    ~Array<Type>();
+    
+    template <class Type>
+    Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+    {
+        if (&toAssign != this)
+        {
+            if (size != toAssign.getSize())
+            {
+                delete[] internalArray;
+                size = toAssign.getSize();
+                internalArray = new Type[size];
+            }
+            
+            for (int index = 0; index < size; index++)
+            {
+                internalArray[index] = toAssign[index];
+            }
+        }
+        return *this;
+    }
+    
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator[] (int index);
+    Type operator[] (int index) const;
+    
+    //Methods
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
+private:
+    Type * internalArray;
+    int size;
 }
 
 #endif /* Array_hpp */
