aa0cb2f7786dd6961256d4a2291c76d73c72534a
ZachYlst <ZachYlst@users.noreply.github.com>
2018-03-27 09:33:20 -0600

    Finished BinaryTreeNode header file.

--- DataStructures/Model/Nodes/BinaryTreeNode.hpp
+++ DataStructures/Model/Nodes/BinaryTreeNode.hpp
@@ -14,7 +14,7 @@
 #include "Node.hpp"
 
 template <class Type>
-class BinaryTreeNode
+class BinaryTreeNode : public Node<Type>
 {
 private:
     BinaryTreeNode<Type> * root;
@@ -24,61 +24,65 @@ public:
     BinaryTreeNode();
     BinaryTreeNode(Type data);
     
-    getRoot();
-    getLeft();
-    getRight();
+    BinaryTreeNode<Type> * getRootNode();
+    BinaryTreeNode<Type> * getLeftNode();
+    BinaryTreeNode<Type> * getRightNode();
     
-    void setRoot(BinaryTreeNode<Type> *);
-    void setLeft(BinaryTreeNode<Type> *);
-    void setRight(BinaryTreeNode<Type> *);
+    void setRootNode(BinaryTreeNode<Type> * root);
+    void setLeftNode(BinaryTreeNode<Type> * left);
+    void setRightNode(BinaryTreeNode<Type> * right);
 };
 
 template <class Type>
-BinaryTreeNode<Type> :: BinaryTreeNode()
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
 {
-    
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
 }
 
 template <class Type>
-BinaryTreeNode<Type> :: BinaryTreeNode<Type data>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>(data)
 {
-    
+    root = nullptr;
+    left = nullptr;
+    right = nullptr;
 }
 
 template <class Type>
-BinaryTreeNode<Type> :: getRoot()
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRootNode()
 {
-    
+    return this->root;
 }
 
 template <class Type>
-BinaryTreeNode<Type> :: getLeft()
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeftNode()
 {
-    
+    return this->left;
 }
 
 template <class Type>
-BinaryTreeNode<Type> :: getRight()
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightNode()
 {
-    
+    return this->right;
 }
 
 template <class Type>
-void BinaryTreeNode<Type> :: setRoot(BinaryTreeNode<Type> *)
+void BinaryTreeNode<Type> :: setRootNode(BinaryTreeNode<Type> * newRoot)
 {
-    
+    root = newRoot;
 }
 
 template <class Type>
-void BinaryTreeNode<Type> :: setLeft(BinaryTreeNode<Type> *)
+void BinaryTreeNode<Type> :: setLeftNode(BinaryTreeNode<Type> * left)
 {
-    
+    this->left = left;
 }
 
 template <class Type>
-void BinaryTreeNode<Type> :: setRight(BinaryTreeNode<Type> *)
+void BinaryTreeNode<Type> :: setRightNode(BinaryTreeNode<Type> * right)
 {
-    
+    this->right = right;
 }
 
 #endif /* BinaryTreeNode_hpp */
